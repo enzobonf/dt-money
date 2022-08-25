6c208b434e10d52efd5a3c377a2be8c637b90991
enzobonf <enzobonfx@gmail.com>
2022-08-25 16:10:55 -0300

    feat: adicionando MirageJS (2)

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,15 +1,17 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Container } from './styles';
 
 export function TransactionsTable() {
 
-  useEffect(() => {
+  const [transactions, setTransactions] = useState<any>([]);
 
+  useEffect(() => {
     fetch('http://localhost:3000/api/transactions')
       .then(response => response.json())
-      .then(data => console.log(data));
-
+      .then(data => setTransactions(data));
   }, []);
+
+  console.log(transactions);
 
   return(
     <Container>
