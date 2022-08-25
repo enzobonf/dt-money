c4ddb3dab9dfc6069cb0c82f80d5824e5dc516a0
enzobonf <enzobonfx@gmail.com>
2022-08-25 16:10:43 -0300

    feat: adicionando MirageJS

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,27 @@
+
+import { createServer } from 'miragejs';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+
+createServer({
+  routes(){
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    this.namespace = 'api';
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date()
+        }
+      ]
+    });
+  }
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
