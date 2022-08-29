5b8095aba6bd09a711bfda912e6aa4efaeb97c51
enzobonf <enzobonfx@gmail.com>
2022-08-29 20:20:12 -0300

    feat: funções no utils para formatar moedas e datas

---
+++ src/utils/formatCurrency.ts
@@ -0,0 +1,6 @@
+export const formatCurrency = (value: number, currency = "BRL"): string => {
+  return new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency,
+  }).format(value);
+};
