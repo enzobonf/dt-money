2e426ab6872b68a18a80418bb03dc228dfbf287b
enzobonf <enzobonfx@gmail.com>
2022-08-29 17:26:21 -0300

    feat: finalização layout form/modal

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import { useState } from "react";
 import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
+
+Modal.setAppElement('#root');
 
 export function App() {
 
-  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(true);
 
   function handleOpenNewTransactionModal(){
     setIsNewTransactionModalOpen(true);
@@ -21,12 +24,10 @@ export function App() {
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
 
-      <Modal 
-            isOpen={isNewTransactionModalOpen} 
-            onRequestClose={handleCloseNewTransactionModal}
-          >
-            <h1>Cadastrar transação</h1>
-          </Modal>
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen} 
+        onRequestClose={handleCloseNewTransactionModal} 
+      />
 
       <GlobalStyle />
     </>
